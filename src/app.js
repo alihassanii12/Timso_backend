22836ca7f7c8214967a6d2e61059f873f615559d
alihassanii12 <devbench0@gmail.com>
2026-03-19 12:10:25 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -60,7 +60,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('/(.*)' , cors(corsOptions));
 
 app.use(morgan('dev'));
 app.use(cookieParser());
